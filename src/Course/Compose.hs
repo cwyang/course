872ec2c893d13b5cfbfcb480b9614cbb741003be
Chul-Woong Yang <cwyang@gmail.com>
2016-03-07 15:37:57 +0900

    Monad does not _Compose_

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE InstanceSigs #-}
 
 module Course.Compose where
 
@@ -12,24 +13,40 @@ import Course.Monad
 
 newtype Compose f g a =
   Compose (f (g a))
+  deriving (Show)
+           
+-- $setup
+-- >>> import Course.Core
+-- >>> import Course.List
+-- >>> import Course.Optional
+
+-- | Test
+--
+-- >>> (+1) <$> (Compose (Full 1 :. Full 2 :. Empty :. Nil) ::Compose List Optional Int)
+-- Compose [Full 2,Full 3,Empty]
+-- >>> pure 3 :: Compose List Optional Int
+-- Compose [Full 3]
+-- >>> Compose (Full (+1):.Full(*2):.Nil) <*> Compose (Full 1 :. Full 2 :. Empty :. Nil)
+-- Compose [Full 2,Full 3,Empty,Full 2,Full 4,Empty]
 
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  (<$>) :: (a -> b) -> Compose f g a -> Compose f g b
+  f <$> Compose g =
+    Compose $ (f <$>) <$> g
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
--- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
--- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  pure x =
+    Compose (pure (pure x))
+  (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+  Compose f <*> Compose g =
+    Compose (lift2 (<*>) f g)
 
 instance (Monad f, Monad g) =>
   Monad (Compose f g) where
 -- Implement the (=<<) function for a Monad instance for Compose
-  (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+  (=<<) :: a -> Compose f g b -> Compose f g a -> Compose f g b
+  f =<< Compose g =
+    error "why this is impossible?" 
